715e645672b7152ce3ed93d90931e9e8bc772e0a
Erik Tollerud <erik.tollerud@gmail.com>
2026-05-14 01:06:26 -0400

    fix sed error

--- build_files/update-image-info.sh
+++ build_files/update-image-info.sh
@@ -13,12 +13,12 @@ sed -i 's|"image-tag": [^,]*|"image-tag": "latest"|' $IMAGE_INFO
 sed -i 's|"image-branch": [^,]*|"image-branch": ""|' $IMAGE_INFO
 
 # OS Release File
-sed -i 's/^VARIANT_ID=.*/VARIANT_ID=$IMAGE_NAME/' /usr/lib/os-release
+sed -i 's/^VARIANT_ID=.*/VARIANT_ID="'"$IMAGE_NAME"'"/' /usr/lib/os-release
 sed -i 's/^ID=bazzite/ID=floofyos/' /usr/lib/os-release
 sed -i 's/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME="floofyos"/' /usr/lib/os-release
 sed -i 's/^PRETTY_NAME=.*/PRETTY_NAME="FloofyOS"/' /usr/lib/os-release
-sed -i 's/^HOME_URL=.*/HOME_URL="https://github.com/eteq/floofyos"/' /usr/lib/os-release
+sed -i 's/^HOME_URL=.*/HOME_URL="https:\/\/github.com\/eteq\/floofyos"/' /usr/lib/os-release
 
 # KDE About page
-sed -i "s|^Website=.*|Website=https://github.com/eteq/floofyos|" /etc/xdg/kcm-about-distrorc
-sed -i "s/^Variant=.*/Variant=Floofyos/" /etc/xdg/kcm-about-distrorc
+sed -i "s|^Website=.*|Website=https:\/\/github.com\/eteq\/floofyos|" /etc/xdg/kcm-about-distrorc
+sed -i "s/^Variant=.*/Variant=$IMAGE_NAME/" /etc/xdg/kcm-about-distrorc
